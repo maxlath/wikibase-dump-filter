0051ac349f04fa21b11e2a2ed9c30995d7986cb9
maxlath <k@maxlath.eu>
2017-03-30 18:29:46 +0200

    added general tests to avoid reproducing 282be58

---
+++ test/general.js
@@ -0,0 +1,13 @@
+require('should')
+const { exec } = require('child_process')
+
+describe('general', function () {
+  it('should return do its job', function (done) {
+    exec('cat ./test/fixtures/entities | ./bin/wikidata-filter -k id -c P31:Q5', (err, stdout, stderr) => {
+      if (err) return done(err)
+      JSON.parse(stdout.split('\n')[0]).id.should.equal('Q23')
+      JSON.parse(stdout.split('\n')[1]).id.should.equal('Q42')
+      done()
+    })
+  })
+})
