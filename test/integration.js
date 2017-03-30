bac1ac077f305bd221e71cbaefbaa8ec567215e1
maxlath <k@maxlath.eu>
2017-03-30 19:25:35 +0200

    renamed general tests > integration tests

--- test/integration.js
+++ test/integration.js
@@ -1,8 +1,8 @@
 require('should')
 const { exec } = require('child_process')
 
-describe('general', function () {
-  it('should return do its job', function (done) {
+describe('integration', function () {
+  it('should do as expected', function (done) {
     exec('cat ./test/fixtures/entities | ./bin/wikidata-filter -k id -c P31:Q5', (err, stdout, stderr) => {
       if (err) return done(err)
       JSON.parse(stdout.split('\n')[0]).id.should.equal('Q23')
